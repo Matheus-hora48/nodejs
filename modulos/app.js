c1ece2b5823527bd6631b9eb5afa5dd113b31339
Matheus Hora <matheus11hora@gmail.com>
2022-04-29 11:08:54 -0300

    Novos

---
+++ modulos/app.js
@@ -0,0 +1,8 @@
+const path = require('path')
+console.log(path.resolve(__dirname, '..'))
+
+//vai recuperar a função  e podemos mexer nela aqui
+// pra frente ./ pra tras ../
+//const multiplicacao = require('./b/c/d/mod')
+
+//console.log(multiplicacao(2, 5))
---
+++ modulos/b/c/d/mod.js
@@ -0,0 +1,4 @@
+//serve para exporta diretamente uma função
+module.exports = function(x, y){
+  return x * y
+}
