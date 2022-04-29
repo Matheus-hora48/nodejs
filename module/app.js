bfc8826b1cdac0814fe8630efd74da18862e2b96
Matheus Hora <matheus11hora@gmail.com>
2022-04-29 10:43:02 -0300

    Novos

---
+++ module/app.js
@@ -0,0 +1,6 @@
+const path = require ('path')
+const axios = require('axios')
+const { Pessoa } = require('./main')
+
+const p1 = new Pessoa('Matheus')
+console.log(p1)
---
+++ module/main.js
@@ -0,0 +1,22 @@
+class Pessoa {
+  constructor(nome) {
+    this.nome = nome;
+  }
+}
+exports.Pessoa = Pessoa;
+
+
+/*
+const nome = 'Mathues'
+const sobrenome = 'Hora'
+
+const falanome = () =>{
+  console.log(nome, sobrenome)
+}
+
+//module.exports.nome = nome
+//module.exports.sobrenome = sobrenome
+
+exports.nome = nome
+exports.sobrenome = sobrenome
+*/
