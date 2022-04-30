4b7a5e6e29cd8aa8c9cdbf060d0921a7a0e895a8
Matheus Hora <matheus11hora@gmail.com>
2022-04-30 19:07:08 -0300

    Novos

--- lendo_escrevendo_arquivos/app.js
+++ lendo_escrevendo_arquivos/app.js
@@ -1,11 +1,25 @@
 const path = require('path');
-const caminhoArquivo = path.resolve(__dirname,'teste.json')
+const caminhoArquivo = path.resolve(__dirname, 'teste.json');
+const escreve = require('./modules/escrever');
+const ler = require('./modules/ler');
 
-const pessoa = [
-  {nome: 'Matheus'},
-  {nome: 'Luis'},
-  {nome: 'Gustavo'},
-  {nome: 'Fernando'},
-]
+const pessoas = [
+  { nome: 'João' },
+  { nome: 'Maria' },
+  { nome: 'Eduardo' },
+  { nome: 'Luiza' },
+];
+const json = JSON.stringify(pessoas, '', 2);
+escreve(caminhoArquivo, json);
 
-const json = JSON.stringify(pessoa, '', 2)
+async function leArquivo(caminho) {
+  const dados = await ler(caminho);
+  renderizaDados(dados);
+}
+
+function renderizaDados(dados) {
+  dados = JSON.parse(dados);
+  dados.forEach(val => console.log(val.nome));
+}
+leArquivo(caminhoArquivo);
+
--- lendo_escrevendo_arquivos/module/escreve.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs').promises;
-const caminhoArquivo = path.resolve(__dirname,'teste.json')
-
-module.exports = (caminho, dados) =>{
-  fs.writeFile(caminho, dados, { flag: 'w' , })
-}
-
-
