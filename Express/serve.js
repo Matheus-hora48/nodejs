f6587e9f5dbd9515befa5c69ed7946aaecc579d3
Matheus Hora <matheus11hora@gmail.com>
2022-04-30 20:45:51 -0300

    novos

--- Express/serve.js
+++ Express/serve.js
@@ -2,9 +2,27 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome">
+    <button>0lá mundo</button>
+    </form>
+    `);
+    });
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado pelo contato');
+});
 
 app.listen(3000, () =>{
     console.log('Teste')
 })
+
+app.listen (3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
