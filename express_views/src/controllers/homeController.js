12a9b05dc59365f328e71f4fcfa6f452a0a84535
Matheus Hora <matheus11hora@gmail.com>
2022-05-01 11:54:16 -0300

    Novos

--- express_views/src/controllers/homeController.js
+++ express_views/src/controllers/homeController.js
@@ -1,11 +1,6 @@
 //onde vai ficar as coisa que vai aparecer nas rotas
 exports.paginaInicial = (req, res) => {
-    res.send(`
-        <form action="/" method="POST">
-        Nome do cliente: <input type="text" name="nome">
-        <button>0lá mundo</button>
-        </form>
-    `);
+    res.render('index');
 }
 
 exports.trataPost = (req, res) => {
