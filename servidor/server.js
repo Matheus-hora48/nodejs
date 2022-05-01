d15310c3e1890d1264e72464a6e0a75ca2172d05
Matheus Hora <matheus11hora@gmail.com>
2022-05-01 11:03:26 -0300

    Novos

--- servidor/server.js
+++ servidor/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-
+app.use(
+    express.urlencoded
+)
 
 app.get('/', (req, res) => {
 res.send(`
@@ -11,6 +13,14 @@ res.send(`
     </form>
     `);
 });
+
+app.get('/teste', (req, res) =>{
+    //req.body vai aparece as coisa que foram enviada por um formulario na requisição do body (de acordo com o name do form)
+    //req.query as query que vem na url /profiles/3
+    //req.params partes que vem na rota da url /profiles/?chave1= valor
+    //cada uma vai pegar um tipo de url
+    res.send('Olá')
+})
 
 app.post('/', (req, res) => {
 res.send('Recebi o formulário');
