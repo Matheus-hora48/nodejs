5f087733a49536bed30465c5ec89ca0199ff24d3
Matheus Hora <matheus11hora@gmail.com>
2022-05-02 14:10:20 -0300

    Novos

---
+++ express_middlewares/routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const route = express. Router();
+const homeController = require('./src/controllers/homeController');
+
+// Rotas da home - onde vai ficara a referencia para as rotas
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+
+module.exports = route;
